8d1f870fde93c5051a3fb319dcfe189e8ab18a48
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-30 19:13:06 +0700

    int main (){     float nilB,nilM,rerata;     string status;
        cout<<"Masukkan Nilai Matematika = ";
        cin >>nilM;
        cout<<"Masukkan Nilai Bahasa Inggris = ";
        cin >>nilB;
    
        rerata = (nilB+nilM)/2;
    
        if ((rerata >= 60) && (nilM >= 70))
            status = "Lulus";
        else
            status = "Tidak Lulus";
    
        cout << "Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;

--- pemdas.cpp
+++ pemdas.cpp
@@ -83,3 +83,21 @@ int main(){
 }
 
 //logika AND
+
+int main (){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan Nilai Matematika = ";
+    cin >>nilM;
+    cout<<"Masukkan Nilai Bahasa Inggris = ";
+    cin >>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout << "Status Kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
