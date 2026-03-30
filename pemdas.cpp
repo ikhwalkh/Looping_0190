2dda4261154b6e2eeeee82a9698db8c0e43241a2
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-30 19:09:08 +0700

    untuk mendapatkan urutan angka acak yg Berbeda

--- pemdas.cpp
+++ pemdas.cpp
@@ -16,7 +16,7 @@ int main(){
     }
     cout<<endl;
 
-    //untuk mendapatkan urutan angka acak yg berbeda
+    //untuk mendapatkan urutan angka acak yg Berbeda
     srand(time(0));
 
     //perulangan dengan while
