88db6a8c3133322a1dff14f6b28083e30758599b
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-30 19:11:41 +0700

    int main(){     float nilB,nilM,rerata;     string status;
        cout<<"Masukkan Nilai Matematika = ";
        cin >>nilM;
        cout<<"Masukkan Nilai Bahasa Inggris = ";
        cin >>nilB;
    
        rerata = (nilB+nilM)/2;
    
        if (!(rerata < 60) )
            status = "Lulus";
        else
            status = "Tidak Lulus";
    
        cout<<"Status Kelulusan = "<<status<<" , dengan nilai rata-rata = "<<rerata<<endl;
    }

--- pemdas.cpp
+++ pemdas.cpp
@@ -43,3 +43,21 @@ int main(){
 }
 
 //Logika not
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan Nilai Matematika = ";
+    cin >>nilM;
+    cout<<"Masukkan Nilai Bahasa Inggris = ";
+    cin >>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60) )
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout<<"Status Kelulusan = "<<status<<" , dengan nilai rata-rata = "<<rerata<<endl;
+}
