7aa51238a2d6950f5798330ad208b9f86ff8058e
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-30 19:04:37 +0700

    #include <iostream> #include <cstdlib> #include <ctime> using namespace std;

--- pemdas.cpp
+++ pemdas.cpp
@@ -1,8 +1,41 @@
+//Library
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
-
+    int x;
 
     //perulangan dengan for
+    //mencetak tulisan sebanyak 5 kali
+    //perulangan dengan for
+    cout<<"Perulangan for"<<endl;
+    for(int i=0;i<=4;i++){
+        cout<<"Teknologi informasi UMY"<<endl;
+    }
+    cout<<endl;
+
+    //untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //perulangan dengan While
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+
+    cout<<'Bilangan acak while yang terakhir ='<<x <<endl;
+    cout<<endl;
+
+    x = 1 + rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<'Bilangan acak do-while yang terakhir ='<<x <<endl;
+    cout<<endl;
 }
