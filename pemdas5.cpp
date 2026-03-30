c9258e01dba15a4afed56da50fb7eb9d6325edb2
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-30 19:15:47 +0700

    #include <iostream> using namespace std;
    // Deklarasi variable global

--- pemdas5.cpp
+++ pemdas5.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    float skor;
-    char nilai;
-
-    cout << "Masukkan skor yang diperoleh = ";
-    cin >> skor;
-
-    if (skor >= 80) nilai = 'A';
-    else if (skor >= 65) nilai ='B';
-    else if (skor >= 50) nilai ='C';
-    else if (skor >= 35) nilai ='D';
-    else nilai ='E';
-
-    cout << "Nilai yang diperoleh = "<< nilai<<endl;
-}
+// Deklarasi variable global
